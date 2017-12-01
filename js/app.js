b13fa1cad917c99f4ef4a182018ea65206ed1fa2
TemplarKnight01 <juans.cardonac@ecci.edu.co>
2017-11-30 21:10:17 -0500

    Añadiendo solo valores alfabeticos, basados en arreglos

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 	$("#generar").click(function(){
-		const valor = "hola";
+		/*
 		var caracteres = "abcdefghijkmnpqrtuvwxyzABCDEFGHIJKLMNPQRTUVWXYZ2346789@_.-,{}?¡¿=()/&%$#";
   		var contraseña = "";
   		for (i=0; i<10; i++) {
   			contraseña += caracteres.charAt(Math.floor(Math.random()*caracteres.length));
-  		}
-		$("#passgenerator").val(contraseña);
-		/*console.log(Math.floor(Math.random()*caracteres.length))*/
+  		}*/
+  		var alfa_caracteres = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"];
+		new_pass = "";
+		for(y=0;y<4;y++){
+			new_pass += alfa_caracteres[Math.floor(Math.random()*alfa_caracteres.length)];
+		}
+		$("#passgenerator").val(new_pass);
 	})
 })
